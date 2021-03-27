55888637f93a9374680897cdd48471aef3bb4c58
Verstov Danila <diverstov@greenatom.ru>
2021-03-27 17:20:17 +0300

    Ref #00000 Initial commit

---
+++ src/main/java/ru/prostandard/ProstandardApplication.java
@@ -0,0 +1,13 @@
+package ru.prostandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProstandardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProstandardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/prostandard/ProstandardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostandard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProstandardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
