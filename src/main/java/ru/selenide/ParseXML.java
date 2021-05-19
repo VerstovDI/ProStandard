665330b6499a69d4cf25cdc463238bb8d4bece5d
AidarValshin <Aidar_V2000@mail.ru>
2021-05-19 12:22:43 +0300

    Refactored parsing xml to DB

--- src/main/java/ru/selenide/ParseXML.java
+++ src/main/java/ru/selenide/ParseXML.java
@@ -1,8 +1,7 @@
 package ru.selenide;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+
 import org.xml.sax.SAXException;
 import ru.selenide.DB.DAO.*;
 import ru.selenide.DB.domain.*;
@@ -12,26 +11,15 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+
+import static ru.selenide.Utills.ParseXMLUtills.*;
+
 
 public class ParseXML {
     public static void parse(Path path) {
         try {
-            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document document = documentBuilder.parse(path.toString());
             ResourceDAO resourceDAO = new ResourceDAO();
-            Resource resource = resourceDAO.findById(1);
             StandardsDAO standardsDAO = new StandardsDAO();
-            Standard standard = getStandard(document, resource);
-            standardsDAO.save(standard);
-
-            saveEmploymentGroupOKZ(document, standard);
-            saveEmploymentGroupOkved(document, standard);
-
             GeneralizedWorkFunctionsDAO generalizedWorkFunctionsDAO = new GeneralizedWorkFunctionsDAO();
             PossibleJobTitlesDAO possibleJobTitlesDAO = new PossibleJobTitlesDAO();
             EducationalRequirementsDAO educationalRequirementsDAO = new EducationalRequirementsDAO();
@@ -39,67 +27,19 @@ public class ParseXML {
             LaborActionsDAO laborActionsDAO = new LaborActionsDAO();
             RequiredSkillsDAO requiredSkillsDAO = new RequiredSkillsDAO();
             NecessaryKnowledgeDAO necessaryKnowledgeDAO = new NecessaryKnowledgeDAO();
+            EmploymentGroupOkvedDAO employmentGroupOkvedDAO = new EmploymentGroupOkvedDAO();
+            EmploymentGroupOkzDAO employmentGroupOkzDAO = new EmploymentGroupOkzDAO();
 
-            NodeList generalizedWorkFunctions = document.getDocumentElement().getElementsByTagName("GeneralizedWorkFunction");
-            //перебор обобщенных трудовых функций
-            for (int i = 0; i < generalizedWorkFunctions.getLength(); i++) {
-                NodeList generalizedWorkFunctionChildNodes = generalizedWorkFunctions.item(i).getChildNodes();
-                if (generalizedWorkFunctionChildNodes != null) {
-                    GeneralizedWorkFunction generalizedWorkFunction = new GeneralizedWorkFunction(generalizedWorkFunctionChildNodes.item(0).getTextContent(),
-                            generalizedWorkFunctionChildNodes.item(1).getTextContent(), Integer.parseInt(generalizedWorkFunctionChildNodes.item(2).getTextContent())
-                            , standard);
-                    generalizedWorkFunctionsDAO.save(generalizedWorkFunction);
-                    NodeList possibleJobTitles = generalizedWorkFunctionChildNodes.item(3).getChildNodes();
-                    if (possibleJobTitles != null) {
-                        for (int j = 0; j < possibleJobTitles.getLength(); j++) {
-                            PossibleJobTitle possibleJobTitle = new PossibleJobTitle(possibleJobTitles.item(j).getTextContent(), generalizedWorkFunction);
-                            possibleJobTitlesDAO.save(possibleJobTitle);
-                        }
-                    }
-                    NodeList educationalRequirements = generalizedWorkFunctionChildNodes.item(4).getChildNodes();
-                    if (educationalRequirements != null) {
-                        for (int j = 0; j < educationalRequirements.getLength(); j++) {
-                            EducationalRequirement educationalRequirement = new EducationalRequirement(educationalRequirements.item(j).getTextContent(), generalizedWorkFunction);
-                            educationalRequirementsDAO.save(educationalRequirement);
-                        }
-                    }
-                    NodeList particularWorkFunctions = generalizedWorkFunctionChildNodes.item(9).getChildNodes();
-                    if (particularWorkFunctions != null) {
-                        for (int j = 0; j < particularWorkFunctions.getLength(); j++) {
-                            NodeList particularWorkFunctionNodes = particularWorkFunctions.item(j).getChildNodes();
-                            if (particularWorkFunctionNodes != null) {
-                                ParticularWorkFunction particularWorkFunction = new ParticularWorkFunction(particularWorkFunctionNodes.item(0).getTextContent(),
-                                        particularWorkFunctionNodes.item(1).getTextContent(),
-                                        Integer.parseInt(particularWorkFunctionNodes.item(2).getTextContent())
-                                        , generalizedWorkFunction);
-                                particularWorkFunctionsDAO.save(particularWorkFunction);
-                                NodeList laborActions = particularWorkFunctionNodes.item(3).getChildNodes();
-                                if (laborActions != null) {
-                                    for (int laborActionsCount = 0; laborActionsCount < laborActions.getLength(); laborActionsCount++) {
-                                        LaborAction laborAction = new LaborAction(laborActions.item(laborActionsCount).getTextContent(), particularWorkFunction);
-                                        laborActionsDAO.save(laborAction);
-                                    }
-                                }
-                                NodeList requiredSkills = particularWorkFunctionNodes.item(4).getChildNodes();
-                                if (requiredSkills != null) {
-                                    for (int requiredSkillsCount = 0; requiredSkillsCount < requiredSkills.getLength(); requiredSkillsCount++) {
-                                        RequiredSkill requiredSkill = new RequiredSkill(requiredSkills.item(requiredSkillsCount).getTextContent(), particularWorkFunction);
-                                        requiredSkillsDAO.save(requiredSkill);
-                                    }
-                                }
-                                NodeList necessaryKnowledgeList = particularWorkFunctionNodes.item(5).getChildNodes();
-                                if (necessaryKnowledgeList != null) {
-                                    for (int necessaryKnowledgeListCount = 0; necessaryKnowledgeListCount < necessaryKnowledgeList.getLength(); necessaryKnowledgeListCount++) {
-                                        NecessaryKnowledge necessaryKnowledge = new NecessaryKnowledge(necessaryKnowledgeList.item(necessaryKnowledgeListCount).getTextContent(), particularWorkFunction);
-                                        necessaryKnowledgeDAO.save(necessaryKnowledge);
-                                    }
-                                }
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse(path.toString());
 
-                            }
-                        }
-                    }
-                }
-            }
+            Resource resource = resourceDAO.findById(1);
+            Standard standard = getStandard(document, resource);
+            standardsDAO.save(standard);
+
+            getAndSaveEmploymentGroupOKZ(document, standard,employmentGroupOkzDAO,employmentGroupOkvedDAO);
+            getAndSaveEmploymentGroupOkved(document, standard,employmentGroupOkvedDAO);
+            getAndSaveGeneralizedWorkFunctions(document, standard, generalizedWorkFunctionsDAO, possibleJobTitlesDAO, educationalRequirementsDAO, particularWorkFunctionsDAO, laborActionsDAO, requiredSkillsDAO, necessaryKnowledgeDAO);
 
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
@@ -107,60 +47,6 @@ public class ParseXML {
 
     }
 
-    private static void saveEmploymentGroupOkved(Document document, Standard standard) {
-        Node employmentGroupListOKVED = document.getDocumentElement().getElementsByTagName("EmploymentGroup").item(0).getChildNodes().item(1);
-        if (employmentGroupListOKVED != null) {
-            EmploymentGroupOkvedDAO employmentGroupOkvedDAO = new EmploymentGroupOkvedDAO();
-            NodeList childNodes = employmentGroupListOKVED.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                EmploymentGroupOkved employmentGroupOkved = new EmploymentGroupOkved(childNodes.item(i).getChildNodes().item(0).getTextContent()
-                        , childNodes.item(i).getChildNodes().item(1).getTextContent(), standard);
-                employmentGroupOkvedDAO.save(employmentGroupOkved);
-            }
-        }
-    }
 
-    private static void saveEmploymentGroupOKZ(Document document, Standard standard) {
-        Node employmentGroupListOKZ = document.getDocumentElement().getElementsByTagName("EmploymentGroup").item(0).getChildNodes().item(0);
-        if (employmentGroupListOKZ != null && employmentGroupListOKZ.getNodeName().equals("ListOKZ")) {
-            EmploymentGroupOkzDAO employmentGroupOkzDAO = new EmploymentGroupOkzDAO();
-            NodeList childNodes = employmentGroupListOKZ.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                EmploymentGroupOkz employmentGroupOkz = new EmploymentGroupOkz(Integer.valueOf(childNodes.item(i).getChildNodes()
-                        .item(0).getTextContent().replaceAll("\\.", ""))
-                        , childNodes.item(i).getChildNodes().item(1).getTextContent(), standard);
-                employmentGroupOkzDAO.save(employmentGroupOkz);
-            }
-        } else if (employmentGroupListOKZ.getNodeName().equals("ListOKVED")) {
-            EmploymentGroupOkvedDAO employmentGroupOkvedDAO = new EmploymentGroupOkvedDAO();
-            NodeList childNodes = employmentGroupListOKZ.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                EmploymentGroupOkved employmentGroupOkved = new EmploymentGroupOkved(childNodes.item(i).getChildNodes().item(0).getTextContent()
-                        , childNodes.item(i).getChildNodes().item(1).getTextContent(), standard);
-                employmentGroupOkvedDAO.save(employmentGroupOkved);
-            }
-        }
-    }
-
-    private static Standard getStandard(Document document, Resource resource) {
-        String nameProfessionalStandart = document.getDocumentElement().getElementsByTagName("NameProfessionalStandart")
-                .item(0).getTextContent();
-        Integer registrationNumber = Integer.valueOf(document.getDocumentElement().getElementsByTagName("RegistrationNumber")
-                .item(0).getTextContent().replaceAll("\\.", ""));
-        String orderNumber = document.getDocumentElement().getElementsByTagName("OrderNumber")
-                .item(0).getTextContent();
-        Date dateOfApproval = Date.valueOf(LocalDate.parse(document.getDocumentElement().getElementsByTagName("DateOfApproval")
-                .item(0).getTextContent(), DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.ENGLISH)));
-        String kindProfessionalActivity = document.getDocumentElement().getElementsByTagName("KindProfessionalActivity")
-                .item(0).getTextContent();
-        String codeKindProfessionalActivity = document.getDocumentElement().getElementsByTagName("CodeKindProfessionalActivity")
-                .item(0).getTextContent();
-        String purposeKindProfessionalActivity = document.getDocumentElement().getElementsByTagName("PurposeKindProfessionalActivity")
-                .item(0).getTextContent().replaceAll("<br />", "");
-        Date dateOfDownloading = Date.valueOf(new SimpleDateFormat("yyyy-MM-dd").format(new java.util.Date()));
-        Standard standard = new Standard(codeKindProfessionalActivity, dateOfApproval, dateOfDownloading, nameProfessionalStandart
-                , registrationNumber, orderNumber, kindProfessionalActivity, purposeKindProfessionalActivity, resource);
-        return standard;
-    }
 
 }
