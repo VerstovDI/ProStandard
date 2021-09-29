41f2e4b407099d853b3f7afcd02ccc01a63df991
Verstov Danila <diverstov@greenatom.ru>
2021-09-29 15:07:09 +0300

    пример парсинга

--- src/main/java/ru/Aidar/ParsingProffStandards/App.java
+++ src/main/java/ru/Aidar/ParsingProffStandards/App.java
@@ -7,31 +7,13 @@ public class App {
 
 
     public static void main(String[] args) throws InterruptedException {
-        String number1 = "06.021";
-        String number2 = "06.022";
 
-        String number4 = "06.024";
-        String number5 = "06.025";
-        String number6 = "06.026";
-        String number7 = "06.027";
-        String number8 = "06.028";
-        String number9 = "06.029";
-        String number10 = "06.010";
-        String number11 = "06.011";
         String dirToSave = Paths.get("resources").toAbsolutePath().toString();
-        GetAndParseProffStandard.getAndParseStandard(number2, dirToSave);
-     /*   getAndParseStandard(number2, dirToSave);
-        getAndParseStandard(number4, dirToSave);
-        getAndParseStandard(number5, dirToSave);
-        getAndParseStandard(number6, dirToSave);
-        getAndParseStandard(number7, dirToSave);
 
-        getAndParseStandard(number8, dirToSave);
-        getAndParseStandard(number9, dirToSave);
-        getAndParseStandard(number10, dirToSave);
-        getAndParseStandard(number11, dirToSave);
-
-      */
+        for (int i = 9; i <= 110; i++) { // парсим стандарты 24-й категории
+            String numberToParse = "24." + String.format("%03d", i);
+            GetAndParseProffStandard.getAndParseStandard(numberToParse, dirToSave);
+        }
 
     }
 
