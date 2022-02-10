4ad99550c3668ff51447bd07d7ce623105129a2b
Verstov Danila <diverstov@greenatom.ru>
2022-02-10 21:42:03 +0300

    Версия 1 доработки таблиц для сущности компетентностной модели

---
+++ src/main/resources/sql/scripts/creation/competence_model_feature.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS proff.tcl_model_status(  -- таблица-классификатор возможных статусов ЖЦ компетентностной модели
+                                                     status_id serial PRIMARY KEY,  -- уникальный идентификатор статуса
+                                                     status_description varchar(15) UNIQUE -- описание статуса (например, PROJECT, ARCHIVE, ACTIVE)
+);
+
+CREATE TABLE IF NOT EXISTS proff.tcl_proffessional_competence( -- таблица-классификатор проф. компетенций, вводимых методистом
+                                                                 prof_competence_id serial PRIMARY KEY,  -- уникальный идентификатор профессиональной компетенции
+                                                                 competence_name varchar(500) UNIQUE  -- описание компетенции (например, "(ПК-4) Способен разрабатывать, согласовывать и выпускать все виды проектной документации")
+);
+
+CREATE TABLE IF NOT EXISTS proff.tcl_proffessional_task_type(  -- таблица-классификатор типа профессиональной деятельности
+                                                                type_id serial PRIMARY KEY, -- уникальный идентификатор типа профессиональной деятельности
+                                                                type_description varchar(50) UNIQUE -- описание типа профессиональной деятельности (например, "научно-исследовательский", "педагогический". Возможно, пустой вариант - тогда ПК стоит отдельно и не имеет типа задачи)
+);
+
+CREATE TABLE IF NOT EXISTS proff.competence_model(  -- Компетентностная модель
+                                                     model_id serial PRIMARY KEY,  -- уникальный идентификатор модели
+                                                     spec_code_id integer,  -- ссылка на код специальности (ссылается на справочник)
+                                                     status_id integer, -- ссылка на статус модели
+                                                     FOREIGN KEY (spec_code_id) REFERENCES proff.dict_specialization (spec_id)
+                                                         ON DELETE RESTRICT ON UPDATE CASCADE,
+                                                     FOREIGN KEY (status_id) REFERENCES proff.tcl_model_status (status_id)
+                                                         ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS proff.prof_competence(  -- профессиональные компетенции
+                                                    model_id integer,  -- ссылка на компетентностную модель
+                                                    competence_id integer,  -- ссылка на профессиональную компетенцию
+                                                    prof_task_type_id integer, -- ссылка на тип задачи профессиональной деятельности
+                                                    knowledge_id integer, -- ссылка на знание
+                                                    skill_id integer, -- ссылка на умение
+                                                    labor_action_id integer, -- ссылка на навык
+                                                    PRIMARY KEY(model_id, competence_id, prof_task_type_id, knowledge_id, skill_id, labor_action_id),
+                                                    FOREIGN KEY (model_id) REFERENCES proff.competence_model (model_id)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE,
+                                                    FOREIGN KEY (competence_id) REFERENCES proff.tcl_proffessional_competence(prof_competence_id)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE,
+                                                    FOREIGN KEY (prof_task_type_id) REFERENCES proff.tcl_proffessional_task_type(type_id)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE,
+                                                    FOREIGN KEY (knowledge_id) REFERENCES proff.necessary_knowledge(id_necessary_knowledge)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE,
+                                                    FOREIGN KEY (skill_id) REFERENCES proff.required_skills(id_required_skill)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE,
+                                                    FOREIGN KEY (labor_action_id) REFERENCES proff.labor_actions(id_labor_action)
+                                                        ON DELETE RESTRICT ON UPDATE CASCADE
+);
