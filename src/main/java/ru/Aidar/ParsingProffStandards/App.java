d4a29cda5072e2f628edfa71443efab62ca3829f
Aidar Valshin <Aidar_V2000@mail.ru>
2022-03-07 15:30:46 +0300

    litlle fix

--- src/main/java/ru/Aidar/ParsingProffStandards/App.java
+++ src/main/java/ru/Aidar/ParsingProffStandards/App.java
@@ -5,19 +5,16 @@ import java.nio.file.Paths;
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
-        String dirToSave = Paths.get("resources").toAbsolutePath().toString();
-        String numberToParse = "01." + String.format("%03d", 1);
-        GetAndParseProfStandard.getAndParseStandard(numberToParse, dirToSave);
-       /*
+
         Thread thread1 = new Thread1();
         Thread thread2 = new Thread2();
-        Thread thread3 = new Thread1();
+        Thread thread3 = new Thread3();
 
         thread1.start();
         thread2.start();
         thread3.start();
 
-         */
+
     }
 
 
